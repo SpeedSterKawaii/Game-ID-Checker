7992519774472ad386eb6187ca2e1f53420de102
SpeedSterKawaii <72403623+SpeedSterKawaii@users.noreply.github.com>
2021-09-22 10:48:16 -0400

    Create Game ID.lua

---
+++ Game ID.lua
@@ -0,0 +1,8 @@
+--[[
+  Roblox game checker, this checks if the game id matches.
+  Developed By SpeedSterKawaii, Or the Speed Software team.
+  Link: https://github.com/SpeedSterKawaii/Game-ID-Checker/
+  
+  Written on: September 22, 2021 at 10:45 AM.
+  Modified; the same date it was released?
+]]
